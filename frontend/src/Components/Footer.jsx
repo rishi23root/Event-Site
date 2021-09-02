c47003af9517278716926fa9186be2a6ad9c0089
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-09-02 19:48:05 +0530

    updated frontend

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Css/Footer.css"
+
+function Footer() {
+    return (
+        <div>
+            footer code
+        </div>
+    )
+}
+
+export default Footer
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import "../Css/Login.css"
 
 function Login() {
     // use effect to decide 
