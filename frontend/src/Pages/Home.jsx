ef040da8f065490ab7b5fc89ba811933dec0b0d0
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-10-11 03:56:20 +0530

    updated for animation and login logic

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import Header from '../Components/Header';
+import React, { useEffect } from 'react'
+import { useHistory } from "react-router-dom";
+// import Header from '../Components/Header';
+import firebase from '../config/firebase';
 
 
 function Home() {
-    return (
-        <div >
+    const History = useHistory();
+    // use to redirec the user if not login 
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user === null) {
+                History.push('/Login')
+                return
+            } else {
+                History.push("/Dashboard")
+                return
+            }
+        })
+    }, [])
 
-        </div>
-    )
+    // for the dashboard
+    // useEffect(() => {
+    //     firebase.auth().onAuthStateChanged(user => {
+    //         if (user === null) {
+    //             History.push('/Login')
+    //             return
+    //         }
+    //     })
+    // }, [])
+
+    // logout btn 
+    // firebase.auth().signOut().then(() => {
+    //     // Sign-out successful.
+    // }).catch((error) => {
+    //     // An error happened.
+    // });
+
+
+    return (<></>)
 }
 
 export default Home
