c899921051d35f8334179a1e109ae1c204918c44
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-10-11 06:58:01 +0530

    updated home

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react'
 import { useHistory } from "react-router-dom";
-// import Header from '../Components/Header';
 import firebase from '../config/firebase';
 
 
 function Home() {
     const History = useHistory();
-    // use to redirec the user if not login 
     useEffect(() => {
         firebase.auth().onAuthStateChanged(user => {
             if (user === null) {
@@ -18,25 +16,6 @@ function Home() {
             }
         })
     }, [])
-
-    // for the dashboard
-    // useEffect(() => {
-    //     firebase.auth().onAuthStateChanged(user => {
-    //         if (user === null) {
-    //             History.push('/Login')
-    //             return
-    //         }
-    //     })
-    // }, [])
-
-    // logout btn 
-    // firebase.auth().signOut().then(() => {
-    //     // Sign-out successful.
-    // }).catch((error) => {
-    //     // An error happened.
-    // });
-
-
     return (<></>)
 }
 
