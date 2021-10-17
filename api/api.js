284d733c81025c26da836696b8b81a760d659137
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-10-17 23:01:49 +0530

    updated api

--- api/api.js
+++ api/api.js
@@ -1,14 +1,56 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 
 
-// user 
+// handle all the data for the animaton
 
 
-router.post('/search-users', (req, res) => {
+// database field
+// heading-topic
+// sumary
+// date 
+// room 
+// by
+// viewed by
+// isConfirmed
+
+
+// login handler
+
+// basic pages ROUTES 
+
+// add api rooute then jse 
+
+
+router.get('/', (req, res) => {
+    res.send("this is the way")
 
 })
 
+router.get('/', (req, res) => {
+    res.status(200)
+    res.sendFile(path.join(__dirname, 'views/mess.html'))
+})
 
+router.get('/newLogin', (req, res) => {
+    res.status(200)
+    // res.render('mess.html')
+    // res.sendFile('mess.html')
+    res.sendFile(path.join(__dirname, 'views//mess.html'))
+})
+
+router.get('/newEventRequest', (req, res) => {
+    res.status(200)
+    // res.render('mess.html')
+    // res.sendFile('mess.html')
+    res.sendFile(path.join(__dirname, 'views//mess.html'))
+})
+
+
+router.get('/confirmTheEvent', (req, res) => {
+    res.status(200)
+    // res.render('mess.html')
+    // res.sendFile('mess.html')
+    res.sendFile(path.join(__dirname, 'views//mess.html'))
+})
 
 module.exports = router;
--- api/authemticate.js
+++ api/authemticate.js
@@ -1,8 +1,31 @@
-module.exports = (req, res, next) => {
+const { v4: uuidv4 } = require('uuid');
+const cookieParser = require('cookie-parser');
+
+
+// aser cookies auth 
+// and timestant update with server update
+
+// check validity of the 
+    // cookie 
+    // authentication code
+
+const checkCookie = (req, res, next) => {
     // cookies authenticaiton and passing to the next step if not 
     const { authorization } = req.headers;
     if (!authorization) {
         return res.status(401).json({ error: "you must be logged in" });
     }
+    
     const token = authorization.replace("Bearer ", "");
+}
+
+
+const checkAuth = (req,res,next) =>{
+
+}
+
+
+module.exports = {
+    checkAuth,
+    checkCookie
 }
