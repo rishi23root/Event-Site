a6bd90956472eef806f9a06790efd8a9a293f7f3
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-10-19 19:32:16 +0530

    create base for api

--- api/api.js
+++ api/api.js
@@ -1,44 +1,35 @@
 const router = require("express").Router();
+const {errorHandler,ErrorConstructor} = require('../middleware/errorHandler')
 
 
 // handle all the data for the animaton
 
 
 // database field
-// heading-topic
-// sumary
-// date 
-// room 
-// by
-// viewed by
-// isConfirmed
+    // heading-topic
+    // sumary
+    // date 
+    // room 
+    // by
+    // viewed by
+    // isConfirmed
 
 
 // login handler
 
-// basic pages ROUTES 
 
-// add api rooute then jse 
-
-// email for the mails and update the cookies for the user
-
-// jwt cookie auth for security and all
-
-router.get('/', (req, res) => {
-    res.send("this is the way")
-
-})
-
-router.get('/', (req, res) => {
-    res.status(200)
-    res.sendFile(path.join(__dirname, 'views/mess.html'))
+router.get('/', (req, res,next) => {
+    // res.send("this is the way")
+    next(new ErrorConstructor("This is the way error"))
+    // next()
 })
 
 router.get('/newLogin', (req, res) => {
+    console.log(146)
     res.status(200)
     // res.render('mess.html')
     // res.sendFile('mess.html')
-    res.sendFile(path.join(__dirname, 'views//mess.html'))
+    res.sendFile('path.join(__dirname,')
 })
 
 router.get('/newEventRequest', (req, res) => {
@@ -55,5 +46,11 @@ router.get('/confirmTheEvent', (req, res) => {
     // res.sendFile('mess.html')
     res.sendFile(path.join(__dirname, 'views//mess.html'))
 })
+
+router.get('/*', (req, res,next) => {
+    next(new ErrorConstructor('Not a valid request !! 😟', 404))
+})
+
+router.use(errorHandler);
 
 module.exports = router;
