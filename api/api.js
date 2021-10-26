ead5436cdf99bf3b46013de08627d52a2e013b41
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-10-26 19:28:18 +0530

    updated routes for auth

--- api/api.js
+++ api/api.js
@@ -1,41 +1,42 @@
 const router = require("express").Router();
-const {errorHandler,ErrorConstructor} = require('../middleware/errorHandler')
-
-// handle all the data for the animaton
-
+const { errorHandler, ErrorConstructor } = require('../middleware/errorHandler')
+const { TokenHandler } = require('../api/tokenHandler');
 
 // database field
-    // heading-topic
-    // sumary
-    // date 
-    // room 
-    // by
-    // viewed by
-    // isConfirmed
+// heading-topic
+// sumary
+// date 
+// room
+// by
+// viewed by
+// isConfirmed
 
 
 // login handler
+router.use(TokenHandler)
+
+// router.get('/', (req, res, next) => {
+//     // res.send("this is the way")
+//     next(new ErrorConstructor("This is the way error"))
+//     // next()
+// })
 
 
-router.get('/', (req, res,next) => {
-    // res.send("this is the way")
-    next(new ErrorConstructor("This is the way error"))
-    // next()
+// common request for the user and admin 
+router.post('/newLogin', (req, res) => {
+    // wellcome of a new user by creating cookies for that user 
+    res.status(200)
+    res.send('user Veryfied.')
 })
 
-router.get('/newLogin', (req, res) => {
-    console.log(146)
+router.post('/newEventRequest', (req, res) => {
+    // handle request for the new request from the user
+    // includes authentication of the data , dates and includs the form data in body 
+    // save the data of the user in the server   
     res.status(200)
     // res.render('mess.html')
     // res.sendFile('mess.html')
-    res.sendFile('path.join(__dirname,')
-})
-
-router.get('/newEventRequest', (req, res) => {
-    res.status(200)
-    // res.render('mess.html')
-    // res.sendFile('mess.html')
-    res.sendFile(path.join(__dirname, 'views//mess.html'))
+    res.send('hi this is rishi')
 })
 
 
@@ -46,7 +47,16 @@ router.get('/confirmTheEvent', (req, res) => {
     res.sendFile(path.join(__dirname, 'views//mess.html'))
 })
 
-router.get('/*', (req, res,next) => {
+
+
+// faculty routes only
+// 1. confirmeby take username and 
+
+
+
+
+router.get('/*', (req, res, next) => {
+    // error pages
     next(new ErrorConstructor('Not a valid request !! 😟', 404))
 })
 
