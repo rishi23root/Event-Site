727a55f18b1354bacb8c54795b89d7febde7f7ce
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-11-02 00:50:03 +0530

    api session handleing and tokenization

--- api/tokenHandler.js
+++ api/tokenHandler.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken')
+var CryptoJS = require("crypto-js");
 const secret = process.env.JWT_SECRET || "Testing"
 const { ErrorConstructor } = require('../middleware/errorHandler')
+
+const keyForMessageCypher = process.env.MESSAGECYPHER || "Testing"
 
 // Token handler 
 const TokenHandler = (req, res, next) => {
@@ -28,7 +31,7 @@ const TokenHandler = (req, res, next) => {
             // console.log(req.body.UserEmail);
             res.cookie(
                 'UserEmail',
-                req.body.UserEmail,
+                encrypt(req.body.UserEmail),
                 {
                     expires: new Date(Date.now() + 60 * 60 * 1000)
                 }
@@ -38,7 +41,7 @@ const TokenHandler = (req, res, next) => {
     } else {
         // check the vaildity of the request if request is vaid then pass to the next step
         // to check cookies
-        // console.log(req.cookies['session'])
+        // console.log(decrypt(req.cookies['userEmail']))
         checkToken(req.cookies['session'])
             .then(_ => next())
             .catch(err => {
@@ -83,6 +86,28 @@ const checkToken = (token) => {
 // const test = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoidGVzdCBpdCBvdXQiLCJtZXNzYWdlIjoic28geW91IHBhcnNlIHRva2VuLCB0aGF0IGlzIG5pY2UsIGJ1dCB3aHkgdGhvdWdodCIsImlhdCI6MTYzNDYzNDQzNX0.JYoL5iUnSZA32w8p7AGbmBUvWfePcXR5qRWwDrYdD3Q'
 // checkToken(test)
 //     .then(console.log)
+
+
+const encrypt =(message) => {
+    return CryptoJS.AES.encrypt(
+        message,
+        keyForMessageCypher
+    )
+        .toString();
+}
+
+const decrypt =(ciphertext) => {
+    return CryptoJS.AES.decrypt(
+        ciphertext,
+        keyForMessageCypher
+    )
+        .toString(CryptoJS.enc.Utf8);
+}
+
+// let a = encrypt('i am the best')
+// console.log(a);
+// console.log(decrypt(a));
+
 
 module.exports = {
     TokenHandler,
